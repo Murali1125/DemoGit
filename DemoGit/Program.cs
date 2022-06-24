35f03313e84fa23531300250d95e897ef895443c
Murali Krishna <murali1125s@gmail.com>
2022-06-24 08:47:19 +0530

    cal wage is done

--- DemoGit/Program.cs
+++ DemoGit/Program.cs
@@ -2,9 +2,11 @@
 Console.WriteLine("Hello, World!");
 
 int isPreset = new Random().Next(0, 2);
+const int WagePerHr = 20, dayHrs = 8;
 if(isPreset == 1)
 {
     Console.WriteLine("employee is present");
+    Console.WriteLine(WagePerHr * dayHrs);
 }
 else
 {
