0baacd51e1cbe05716820a7c1b40e1b1d619a478
Murali Krishna <murali1125s@gmail.com>
2022-06-24 08:45:28 +0530

    employee is presnt or abcent

--- DemoGit/Program.cs
+++ DemoGit/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int isPreset = new Random().Next(0, 2);
+if(isPreset == 1)
+{
+    Console.WriteLine("employee is present");
+}
+else
+{
+    Console.WriteLine(" Employee is absent");
+}
